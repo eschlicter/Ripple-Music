bc7f79c692bac39b4ebc2215bd986d1f0eeb7c6f
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2018-03-15 11:33:56 -0500

    assignment player bar buttons complete

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -11,8 +11,8 @@ class PlayerBar extends Component {
         <button id="play-pause" onClick={this.props.handleSongClick} >
           <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
         </button>
-        <button id="next">
-          <span className="ion-skip-forawrd"></span>
+        <button id="next" onClick={this.props.handleNextClick}>
+          <span className="ion-skip-forward"></span>
         </button>
       </section>
       <section id="time-control">
